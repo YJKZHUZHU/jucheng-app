d639fcb6cb93ff26d9f5ee34e36c53aac5d1dfdd
wawawa <2730455209@qq.com>
2018-11-19 23:48:32 +0800

    init again

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {Button} from 'antd-mobile'
 import {HashRouter as Router,Route,Link} from 'react-router-dom'
 import {Provider} from 'react-redux'
+import './index.scss'
 class App extends Component {
   render() {
     return (
@@ -9,11 +10,15 @@ class App extends Component {
         <Router>
           <div className="App">
             <Button type='primary'>enen</Button>
+            <div className='hahha'>演唱会</div>
           </div>
         </Router>
       </Provider>
      
     );
+  }
+  componentDidMount() {
+    
   }
 }
 
